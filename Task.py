3a84e7b1707b4a08707bc2f0b8f3f4ea1e6c5683
Savan Meggi <you@example.comsmeggi@spartaglobal.comgit config --global user.name Your>
2022-07-23 22:06:48 +0100

    Rows missing values have been deleted

--- Task.py
+++ Task.py
@@ -8,14 +8,20 @@ data_file = pd.read_csv('al_results_2020 (1).csv')
 data_file = data_file.drop(columns=['Zscore', 'gender', 'syllabus'])
 
 # saving a modified file .csv
-data_file.to_csv('modified.csv')
+#data_file.to_csv('modified.csv')
 
 # using conditions to find rows with absent values for student with  '|' as a or statement
-student_with_one_or_absents = data_file.loc[ (data_file['sub1_r'] == 'Absent') | (data_file['sub2_r'] == 'Absent') | (data_file['sub3_r'] == 'Absent') | (data_file['cgt_r'] == 'Absent')]
+#student_with_one_or_absents = data_file.loc[ (data_file['sub1_r'] == 'Absent') | (data_file['sub2_r'] == 'Absent') | (data_file['sub3_r'] == 'Absent') | (data_file['cgt_r'] == 'Absent')]
 # saving all the absent data in a file
-student_with_one_or_absents.to_csv('absents.csv')
+#student_with_one_or_absents.to_csv('absents.csv')
+
+#
+
 
 # using conditions to find rows with no absent values for student with  '&' as a and statement
-data_file = data_file.loc[ (data_file['sub1_r'] != 'Absent') & (data_file['sub2_r'] != 'Absent') & (data_file['sub3_r'] != 'Absent') & (data_file['cgt_r'] != 'Absent')]
+data_file = data_file.loc[ (data_file['sub1_r'] != 'Absent') & (data_file['sub2_r'] != 'Absent') & (data_file['sub3_r'] != 'Absent') & (data_file['cgt_r'] != 'Absent') & (data_file['birth_day'] != 'Invalid error') & (data_file['birth_year'] != 'Invalid error') & (data_file['island_rank'] != '-') & (data_file['district_rank'] != '-')]
 # saving a modified file .csv
-data_file.to_csv('modified.csv')
+#data_file.to_csv('modified.csv')
+
+
+# data_file.to_csv('modified.csv')
