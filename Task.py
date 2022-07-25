3148162391f8524f91a65150ea6f12d0285d7b33
Savan Meggi <you@example.comsmeggi@spartaglobal.comgit config --global user.name Your>
2022-07-25 09:55:52 +0100

    Got rid of the low_memory issue

--- Task.py
+++ Task.py
@@ -3,7 +3,7 @@
 import pandas as pd
 
 # Read the csv file
-data_file = pd.read_csv('al_results_2020 (1).csv')
+data_file = pd.read_csv('al_results_2020 (1).csv', low_memory=False)
 # The columns Zscore, gender, and syllabus removed
 data_file = data_file.drop(columns=['Zscore', 'gender', 'syllabus'])
 
