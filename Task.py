c86fc4c647d639f6a010f9c6e16298726fb65d36
Savan Meggi <you@example.comsmeggi@spartaglobal.comgit config --global user.name Your>
2022-07-25 09:53:18 +0100

    Created birth date and removed the 3 columns, still need to clean the format

--- Task.py
+++ Task.py
@@ -23,16 +23,16 @@ data_file = data_file.loc[ (data_file['sub1_r'] != 'Absent') & (data_file['sub2_
 # saving a modified file .csv
 #data_file.to_csv('modified.csv')
 
-months_dict = {'January': '01', 'February': '02', 'March': '03', 'April': '04', 'May': '05', 'June': '06',
-               'July': '07', 'August': '08', 'September': '09', 'October': '10', 'November': '11', 'December': '12'}
+#months_dict = {'January': '01', 'February': '02', 'March': '03', 'April': '04', 'May': '05', 'June': '06',
+ #              'July': '07', 'August': '08', 'September': '09', 'October': '10', 'November': '11', 'December': '12'}
 
-data_file['birth_month'] = data_file['birth_month'].apply(lambda key: months_dict[key])
+#data_file['birth_month'] = data_file['birth_month'].apply(lambda key: months_dict[key])
 
 #  concatenate birth_day, birth_month and birth_day to form new birthdate column
-data_file['birth_date'] = data_file['birth_day'].astype(str) + '-' + data_file['birth_month'] + '-' + data_file[
-    'birth_year'].astype(str)
+data_file['birth_date'] = data_file['birth_day'] + '-' + data_file['birth_month'] + '-' + data_file[
+    'birth_year']
 
-data_file['birth_date'] = pd.to_datetime(data_file['birth_date'], format='%d-%m-%Y', errors='course')
+#data_file['birth_date'] = pd.to_datetime(data_file['birth_date'], format='%d-%m-%Y', errors='course')
 
 data_file = data_file.drop(columns=['birth_day', 'birth_month', 'birth_year'])
-# data_file.to_csv('modified.csv')
+data_file.to_csv('modified.csv')
